3459599aeff8a04cee1ecfef064cee6524a8eec6
Iago Cabral <iagocabral.software@gmail.com>
2024-10-21 22:27:22 -0300

    4 - Projects section v1

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+    {
+      id: 1,
+      title: "React Portfolio Website",
+      description: "Project 1 description",
+      image: "/images/projects/1.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com",
+      previewUrl: "/"
+    },
+    {
+      id: 2,
+      title: "Potography Portfolio Website",
+      description: "Project 2 description",
+      image: "/images/projects/2.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com",
+      previewUrl: "/"
+    },
+    {
+      id: 3,
+      title: "E-commerce Application",
+      description: "Project 3 description",
+      image: "/images/projects/3.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com",
+      previewUrl: "/"      
+    },
+    {
+      id: 4,
+      title: "Food Ordering Application",
+      description: "Project 4 description",
+      image: "/images/projects/4.png",
+      tag: ["All", "Mobile"],
+      gitUrl: "https://github.com",
+      previewUrl: "/"
+    },
+    {
+      id: 5,
+      title: "React Firebase Template",
+      description: "Authentication and CRUD operations",
+      image: "/images/projects/5.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com",
+      previewUrl: "/"
+    },
+    {
+      id: 6,
+      title: "Full-stack Roadmap",
+      description: "Project 5 description",
+      image: "/images/projects/6.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com",
+      previewUrl: "/"
+    },
+  ];
+
+const ProjectsSection = () => {
+    return (
+        <>
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
+            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {projectsData.map((project) => (
+                    <ProjectCard 
+                        key={project.id} 
+                        titlle={project.title}
+                        description={project.description}
+                        imgUrl={project.image} 
+                        gitUrl={project.gitUrl}
+                        previewURL={project.previewUrl}
+                        />
+                        ))};
+                        </div>
+        </>
+    );
+};
+
+export default ProjectsSection;
